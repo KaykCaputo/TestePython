573a975f1f238dac63a5856f352706908ade4982
Kayk Caputo <caputokayk@gmail.com>
2025-02-13 16:43:12 -0300

    initial server implementation with fastapi

--- teste.py
+++ teste.py
@@ -13,6 +13,10 @@ def randomOddNumber():
         if num % 2 != 0:
             return num
         continue
-               
+
+def test_pair():
+    assert randomNumPair() % 2 == 0
+def test_odd():
+    assert randomOddNumber() % 2 != 0
 
 print(randomOddNumber())
